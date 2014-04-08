4bf7246f9a11c45fc42abf516b3d3325a4c86a47
TheBerkin <grenzatuna@gmail.com>
2014-04-08 11:40:57 -0500

    Added branching to RNG

--- Manhood/ManRandom.cs
+++ Manhood/ManRandom.cs
@@ -9,22 +9,48 @@ namespace Manhood
 {
     public class ManRandom
     {
-        public long Seed, Generation;
+        private List<SG> _sg;
+
+        public long Seed
+        {
+            get { return _sg[_sg.Count - 1].Seed; }
+            set { _sg[_sg.Count - 1].Seed = value; }
+        }
+
+        public long Generation
+        {
+            get { return _sg[_sg.Count - 1].Generation; }
+            set { _sg[_sg.Count - 1].Generation = value; }
+        }
+
+        private class SG
+        {
+            public long Seed, Generation;
+
+            public SG(long s, long g)
+            {
+                Seed = s;
+                Generation = g;
+            }
+        }
 
         public ManRandom(long seed)
         {
+            _sg = new List<SG>();
             Seed = seed;
             Generation = 0;
         }
 
         public ManRandom(long seed, long generation)
         {
+            _sg = new List<SG>();
             Seed = seed;
             Generation = generation;
         }
 
         public ManRandom()
         {
+            _sg = new List<SG>();
             Seed = Environment.TickCount;
             Generation = 0;
         }
@@ -81,6 +107,24 @@ namespace Manhood
                 num = GetRaw(num, gens[i]);
             }
             return num;
+        }
+
+        public void Branch(long seed, long generation)
+        {
+            _sg.Add(new SG(seed, generation));
+        }
+
+        public void Branch()
+        {
+            _sg.Add(new SG(Seed, Generation));
+        }
+
+        public void Merge()
+        {
+            if (_sg.Count > 1)
+            {
+                _sg.RemoveAt(_sg.Count - 1);
+            }
         }
 
         public int Peek()
